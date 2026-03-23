96401ba0856c8ca8606fb6dfbfa14b5116031cb3
Shubham Kumar <kumarshubham4041@gmail.com>
2026-03-24 00:07:27 +0530

    Create Lab_01.js

---
+++ Lab_01.js
@@ -0,0 +1,16 @@
+//Build a Java Script Trivia Bot
+
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="Rajua";
+let botLocation = "Bihar";
+let favoriteLanguage = "Python";
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+let codingFact = `My ${favoriteLanguage} is related to the name of a Snake.`;
+console.log(codingFact);
+codingFact = `${favoriteLanguage} is one of the most popular languages on Earth.`;
+console.log(codingFact);
+codingFact= `${favoriteLanguage} is very close to the English Language. It is very easy language.`;
+console.log(codingFact);
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
+
