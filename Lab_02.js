f34dc75a593a9df7bcbd2f2e8a7d016420c0ada6
Shubham Kumar <kumarshubham4041@gmail.com>
2026-03-27 20:27:18 +0530

    Create Lab_02.js

---
+++ Lab_02.js
@@ -0,0 +1,28 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "handsome";
+noun = "Mahatma";
+verb = "play";
+place = "patna";
+adjective2 = "thin";
+noun2 = "Gandhi";
+
+let firstStory =`Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "beautiful";
+noun = "Vallabhbhai";
+verb = "eat";
+place = "bhopal";
+adjective2 = "ugly";
+noun2 = "Jawahar";
+
+let secondStory =`Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
