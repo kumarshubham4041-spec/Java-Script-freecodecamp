aba7a1132f2837a99a561985cd72bb6bf4c9cd2c
Shubham Kumar <kumarshubham4041@gmail.com>
2026-04-04 00:38:04 +0530

    Create Lab_07.js

---
+++ Lab_07.js
@@ -0,0 +1,19 @@
+// Buld a Card Counting Assistant
+
+let count = 0
+function cardCounter(card){
+  
+  if (card>= 2 && card<=6){
+    count++;
+  }
+  
+  else if (card===10 || card=== "J"||card==="Q"||card==="K"||card==="A"){
+    count--;
+  }
+  if (count>0){
+    return count+" Bet";
+  }
+  else{
+    return count+" Hold";
+  }
+}
